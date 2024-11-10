e7d8ca32203dddd1391ba12f0b35646ff370a886
ingar2005 <muhammadingar05@gmail.com>
2024-11-10 14:10:22 +0000

    create suppliers table

--- main.go
+++ main.go
@@ -3,31 +3,85 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
-func testConnection(db_user string, db_pass string, db_name string, db_endpoint string) {
+type Supplier struct {
+	supplierID          int64
+	supplier_name       string
+	supplier_contact_no string
+	lead_time           int64
+	monday_deliver      bool
+	tuesday_deliver     bool
+	wednesday_deliver   bool
+	thursday_deliver    bool
+	friday_deliver      bool
+	saturday_deliver    bool
+	sunday_deliver      bool
+}
+
+const (
+	createSuppliers = (`
+		CREATE TABLE IF NOT EXISTS suppliers (supplierID int NOT NULL AUTO_INCREMENT,
+		supplier_name varchar(255) ,
+	    supplier_contact_no varchar(255),
+	    lead_time int,
+	    monday_deliver boolean,
+	    tuesday_deliver boolean,
+	    wednesday_deliver boolean,
+	    thursday_deliver boolean,
+	    friday_deliver boolean,
+	    saturday_deliver boolean,
+	    sunday_deliver boolean,
+	    PRIMARY KEY (supplierID));`)
+	genericSupplier = `
+		INSERT IGNORE INTO suppliers
+		(supplierID, supplier_name, lead_time,
+	 	monday_deliver, tuesday_deliver, wednesday_deliver, thursday_deliver, friday_deliver, saturday_deliver, sunday_deliver)
+		VALUES (1, 'generic', 0, 1, 1, 1, 1, 1, 1, 1);
+	`
+)
+
+func connection(db_user string, db_pass string, db_name string, db_endpoint string) (*sql.DB, error) {
 	var dsn string = fmt.Sprintf("%s:%s@tcp(%s)/%s", db_user, db_pass, db_endpoint, db_name)
 	db, err := sql.Open("mysql", dsn)
 	fmt.Println("attempting to connect to the database")
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
-	defer db.Close()
+
 	err = db.Ping()
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 	fmt.Println("Successfully connected to the database")
+
 	var version string
 	err = db.QueryRow("SELECT VERSION()").Scan(&version)
 	if err != nil {
 		panic(err.Error())
 	}
 	fmt.Println("Database version: ", version)
+
+	return db, nil
+}
+func initialiseTables(db *sql.DB) (err error) {
+
+	_, err = db.Exec(createSuppliers)
+	if err != nil {
+		return (err)
+
+	}
+	_, err = db.Exec(genericSupplier)
+	if err != nil {
+		return (err)
+	}
+
+	return nil
 }
 
 func main() {
@@ -37,6 +91,36 @@ func main() {
 	db_name := os.Getenv("DB_NAME")
 	db_endpoint := os.Getenv("DB_ENDPOINT")
 
-	testConnection(db_user, db_pass, db_name, db_endpoint)
+	db, err := connection(db_user, db_pass, db_name, db_endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
+	err = initialiseTables(db)
+
+	row, err := db.Query("SELECT * FROM suppliers")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+
+	for row.Next() {
+		var contactNo sql.NullString
+		data := Supplier{}
+
+		err := row.Scan(&data.supplierID, &data.supplier_name, &contactNo, &data.lead_time,
+			&data.monday_deliver, &data.tuesday_deliver, &data.wednesday_deliver, &data.thursday_deliver,
+			&data.friday_deliver, &data.saturday_deliver, &data.sunday_deliver)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if contactNo.Valid {
+			data.supplier_contact_no = contactNo.String
+		} else {
+			data.supplier_contact_no = "N/A"
+		}
+		fmt.Println(data)
+	}
 }
