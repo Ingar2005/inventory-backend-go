dedaf97d00a441aba8446f1cb8759657cb231c15
ingar2005 <muhammadingar05@gmail.com>
2024-11-11 11:31:40 +0000

    add rooms table with generic room

--- main.go
+++ main.go
@@ -23,29 +23,67 @@ type Supplier struct {
 	saturday_deliver    bool
 	sunday_deliver      bool
 }
+type room struct {
+	roomId   int
+	roomName string
+}
 
 const (
-	createSuppliers = (`
-		CREATE TABLE IF NOT EXISTS suppliers (supplierID int NOT NULL AUTO_INCREMENT,
-		supplier_name varchar(255) ,
-	    supplier_contact_no varchar(255),
-	    lead_time int,
-	    monday_deliver boolean,
-	    tuesday_deliver boolean,
-	    wednesday_deliver boolean,
-	    thursday_deliver boolean,
-	    friday_deliver boolean,
-	    saturday_deliver boolean,
-	    sunday_deliver boolean,
-	    PRIMARY KEY (supplierID));`)
+	createSuppliers = `
+	CREATE TABLE IF NOT EXISTS suppliers (supplierID int NOT NULL AUTO_INCREMENT,
+		supplierName varchar(255) NOT NULL ,
+	    supplierContact_no varchar(255) ,
+	    leadTime int  NOT NULL  ,
+	    mondayDeliver boolean  NOT NULL ,
+	    tuesdayDeliver boolean  NOT NULL,
+	    wednesdayDeliver boolean  NOT NULL,
+	    thursdayDeliver boolean  NOT NULL,
+	    fridayDeliver boolean  NOT NULL,
+	    saturdayDeliver boolean  NOT NULL,
+	    sundayDeliver boolean  NOT NULL,
+	    PRIMARY KEY (supplierID));
+					`
 	genericSupplier = `
 		INSERT IGNORE INTO suppliers
-		(supplierID, supplier_name, lead_time,
-	 	monday_deliver, tuesday_deliver, wednesday_deliver, thursday_deliver, friday_deliver, saturday_deliver, sunday_deliver)
+		(supplierID, supplierName, leadTime,
+			mondayDeliver, tuesdayDeliver, wednesdayDeliver, thursdayDeliver, fridayDeliver, saturdayDeliver, sundayDeliver)
 		VALUES (1, 'generic', 0, 1, 1, 1, 1, 1, 1, 1);
+		`
+	createRooms = `
+	CREATE TABLE rooms(
+	roomID int NOT NULL AUTO_INCREMENT,
+	roomName varchar(255) NOT NULL,
+	primary key(roomID));
+	`
+
+	genericRoom = `
+	INSERT IGNORE INTO rooms(roomID,roomName) VALUES (1,'generic');
 	`
 )
 
+func main() {
+	var err error
+
+	godotenv.Load()
+	db_user := os.Getenv("DB_USER")
+	db_pass := os.Getenv("DB_PASSWORD")
+	db_name := os.Getenv("DB_NAME")
+	db_endpoint := os.Getenv("DB_ENDPOINT")
+
+	// CRETE A CONNECTION
+	db, err := connection(db_user, db_pass, db_name, db_endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// CREATE TABLES AND INSERT GENERIC VALUES
+	err = initialiseTables(db)
+
+	//PRINT TABLES
+	printSuppliers(db)
+	printRooms(db)
+}
 func connection(db_user string, db_pass string, db_name string, db_endpoint string) (*sql.DB, error) {
 	var dsn string = fmt.Sprintf("%s:%s@tcp(%s)/%s", db_user, db_pass, db_endpoint, db_name)
 	db, err := sql.Open("mysql", dsn)
@@ -74,34 +112,29 @@ func initialiseTables(db *sql.DB) (err error) {
 	_, err = db.Exec(createSuppliers)
 	if err != nil {
 		return (err)
-
 	}
+
 	_, err = db.Exec(genericSupplier)
+	if err != nil {
+		return (err)
+	}
+
+	_, err = db.Exec(createRooms)
+	if err != nil {
+		return (err)
+	}
+
+	_, err = db.Exec(genericRoom)
 	if err != nil {
 		return (err)
 	}
 
 	return nil
 }
-
-func main() {
-	godotenv.Load()
-	db_user := os.Getenv("DB_USER")
-	db_pass := os.Getenv("DB_PASSWORD")
-	db_name := os.Getenv("DB_NAME")
-	db_endpoint := os.Getenv("DB_ENDPOINT")
-
-	db, err := connection(db_user, db_pass, db_name, db_endpoint)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	err = initialiseTables(db)
-
+func printSuppliers(db *sql.DB) (err error) {
 	row, err := db.Query("SELECT * FROM suppliers")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer row.Close()
 
@@ -113,7 +146,7 @@ func main() {
 			&data.monday_deliver, &data.tuesday_deliver, &data.wednesday_deliver, &data.thursday_deliver,
 			&data.friday_deliver, &data.saturday_deliver, &data.sunday_deliver)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 
 		if contactNo.Valid {
@@ -123,4 +156,24 @@ func main() {
 		}
 		fmt.Println(data)
 	}
+	return nil
+}
+func printRooms(db *sql.DB) (err error) {
+
+	rows, err := db.Query("SELECT * FROM rooms")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var data room = room{}
+		err = rows.Scan(&data.roomId, &data.roomName)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(data)
+	}
+	return nil
 }
